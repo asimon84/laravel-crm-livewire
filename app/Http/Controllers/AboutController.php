da51420380fb57838f9f247fab180c795a9c3b59
asimon84 <ajsimon4@yahoo.com>
2025-11-15 19:10:28 -0800

    about page added

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Show the view for the About page
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(Request $request) {
+        return view('about', []);
+    }
+}
